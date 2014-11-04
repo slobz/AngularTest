d0f2998405320630eb452444c27009aeb52f221d
Seb <seb@seb-G53SW.(none)>
2014-11-04 22:58:44 +0100

    Debut ajout des onglets

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -12,47 +12,64 @@
     var app = angular.module('collection', []);
 
     // Controleur
-    app.controller('CollectionController', [ '$http',function($http) {
-            
-        var liste = this;
-        this.i = 0;
-        
-        liste.product = [];
-            
-        $http.get('http://localhost/WS/Albums/album.php').success(function(data){
-            liste.product = data;
-            
-        });
-        
-        //======================================================================
-        // Fonctions
-        this.inc = function(){
-            this.i < this.product.length - 1? this.i++ :this.i = 0;
-        }; 
-     
-        this.desc = function(){
-            this.i > 0 ? this.i-- :this.i = this.product.length - 1;
-        };
-        
-        this.estFini = function(fini){
-            return (fini == 1);
-        };
-        
-        this.estAJour = function(possede,total){
-            return (possede == total);
-        };
-        
-        this.addTome = function(tome){
-            $http.post('http://localhost/WS/Albums/album.php',{action:'add', titre:tome.titre}).success(function(data){
-               console.log(data); 
-               tome.possede++;
-            }).error(function(){
-                liste.error = true;
-                liste.error_text = 'Erreur lors de l\'ajout';
+    app.controller('CollectionController', ['$http', function($http) {
+
+            var liste = this;
+            this.i = 0;
+
+            liste.product = [];
+
+            $http.get('http://localhost/WS/Albums/album.php').success(function(data) {
+                liste.product = data;
+
             });
+
+            //======================================================================
+            // Fonctions
+            this.inc = function() {
+                this.i < this.product.length - 1 ? this.i++ : this.i = 0;
+            };
+
+            this.desc = function() {
+                this.i > 0 ? this.i-- : this.i = this.product.length - 1;
+            };
+
+            this.estFini = function(fini) {
+                return fini === 1;
+            };
+
+            this.estAJour = function(possede, total) {
+                return possede === total;
+            };
+
+            this.addTome = function(tome) {
+                $http.post('http://localhost/WS/Albums/album.php', {action: 'add', titre: tome.titre}).success(function(data) {
+                    console.log(data);
+                    tome.possede++;
+                }).error(function() {
+                    liste.error = true;
+                    liste.error_text = 'Erreur lors de l\'ajout';
+                });
+            };
+
+            //======================================================================
+
+        }]);
+
+    // Controleur des tab
+    app.controller('PanelController', function() {
+
+        this.tab = 1;
+
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
         };
-        
-        //======================================================================
-        
-    }]);
+
+        this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+        };
+
+
+    });
+
 })();
